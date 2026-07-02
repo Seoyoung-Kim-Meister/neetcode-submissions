fac9cdaa640f81ff4530ff093ccd587a513c8e26
Seoyoung Kim <seoyoungkim.math@gmail.com>
2026-07-02 16:25:33 +0200

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.replace(" ","")
+        s=s.replace("?","")
+        s= ''.join(char for char in s if char.isalnum())
+        s=s.lower()
+        t=s[::-1]
+        return t==s
